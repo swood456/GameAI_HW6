0913873539cc831ed4579ef78393ccffb360c738
swood456 <crazedsportsfan@gmail.com>
2017-11-05 13:54:50 -0500

    set up a 2d array of floats that corresponds to the percentage that each tile is blocked

--- Assets/Scripts/CreateWorldFromMap.cs
+++ Assets/Scripts/CreateWorldFromMap.cs
@@ -22,19 +22,31 @@ public class CreateWorldFromMap : MonoBehaviour {
     private float image_square_size;
 
     [SerializeField]
-    private int num_squares_per_tile;
+    private int num_squares_per_tile_x;
+
+    [SerializeField]
+    private int num_squares_per_tile_y;
 
     string type;
     int height;
     int width;
+    int tile_map_width;
+    int tile_map_height;
     GameObject map_root;
+    //bool[,] sqaure_map;
+    float[,] map_representation;
 
     // Use this for initialization
     void Start () {
         map_root = new GameObject("Map_Root");
         CreateMap();
+
 	}
 	
+    private void CreateTileGrid()
+    {
+    }
+
     private void CreateMap()
     {
         string[] lines = input_map.text.Split('\n');
@@ -42,25 +54,31 @@ public class CreateWorldFromMap : MonoBehaviour {
         type = lines[0].Split(' ')[1];
         height = int.Parse(lines[1].Split(' ')[1]);
         width = int.Parse(lines[2].Split(' ')[1]);
-        
+
+        tile_map_width = width % num_squares_per_tile_x == 0 ? width / num_squares_per_tile_x : width / num_squares_per_tile_x + 1;
+        tile_map_height = height % num_squares_per_tile_y == 0 ? height / num_squares_per_tile_y : height / num_squares_per_tile_y + 1;
+
+        map_representation = new float[tile_map_width, tile_map_height];
+        int tile_area = num_squares_per_tile_x * num_squares_per_tile_y;
 
         float left_side = -width * image_square_size / 2;
         float top = height * image_square_size / 2;
-        for(int j = 4; j < lines.Length; ++j)
+        for (int j = 4; j < lines.Length; ++j)
         {
-            //print("doing row " + (j-3) +" of size " + lines[j].Length);
             for(int i = 0; i < width; ++i )
             {
                 
                 GameObject to_spawn;
-
+                int m_x = i / num_squares_per_tile_x, m_y = (j-4) / num_squares_per_tile_y;
                 switch(lines[j][i])
                 {
                     case '@':
                         to_spawn = void_square;
+                        map_representation[m_x, m_y] += 1.0f / tile_area;
                         break;
                     case 'T':
                         to_spawn = tree_square;
+                        map_representation[m_x, m_y] += 1.0f / tile_area;
                         break;
                     default:
                         to_spawn = empty_square;
@@ -70,5 +88,13 @@ public class CreateWorldFromMap : MonoBehaviour {
                 Instantiate(to_spawn, new Vector3(left_side + i * image_square_size, top - (j-4) * image_square_size, 0), Quaternion.identity, map_root.transform);
             }
         }
+
+        //for(int j = 0; j < tile_map_width; ++j)
+        //{
+        //    for(int i = 0; i < tile_map_height; ++i)
+        //    {
+        //        print(map_representation[i,j]);
+        //    }
+        //}
     }
 }
