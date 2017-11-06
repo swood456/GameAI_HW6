53e0c83889c0e7a4582883a699bc647d6c19795a
swood456 <woods5@win.rpi.edu>
2017-11-06 15:45:23 -0500

    added a very broken solution for doing a*, will fix after class

--- Assets/Scripts/CreateWorldFromMap.cs
+++ Assets/Scripts/CreateWorldFromMap.cs
@@ -4,6 +4,40 @@ using System.IO;
 using System.Text;
 using UnityEngine;
 
+public struct Point
+{
+    public int x;
+    public int y;
+    public float f;
+    public float g;
+    public float h;
+    // TODO: parnets | C# doesn't like a point having a point variable :(
+    public Point(int _x, int _y, float _f = 0.0f, float _g = 0.0f, float _h = 0.0f)
+    {
+        x = _x;
+        y = _y;
+        f = _f;
+        g = _g;
+        h = _h;
+    }
+
+    // equality nonsense | maybe don't need??
+    //public override bool Equals(object obj)
+    //{
+    //    return obj is Point && this == (Point)obj;
+    //}
+
+    //public static bool operator ==(Point a, Point b)
+    //{
+    //    return a.x == b.x && a.y == b.y;
+    //}
+
+    //public static bool operator !=(Point a, Point b)
+    //{
+    //    return !(a == b);
+    //}
+}
+
 public class CreateWorldFromMap : MonoBehaviour {
 
     [SerializeField]
@@ -78,10 +112,11 @@ public class CreateWorldFromMap : MonoBehaviour {
         }
     }
 
-    public void FindPath()
+    public List<Point> FindPath()
     {
+        List<Point> path = new List<Point>();
         if (!start_placed || !end_placed)
-            return;
+            return path;
 
         // first, locate what square the start and end is in
         int s_i, s_j, e_i, e_j;
@@ -95,31 +130,168 @@ public class CreateWorldFromMap : MonoBehaviour {
         if(s_i < 0 || s_i >= width || s_j < 0 || s_j >= height)
         {
             print("error: placed start tile off map");
-            return;
+            return path;
         }
 
         if (e_i < 0 || e_i >= width || e_j < 0 || e_j >= height)
         {
             print("error: placed end tile off map");
-            return;
+            return path;
         }
 
         int s_x = s_i / num_squares_per_tile_x, s_y = s_j / num_squares_per_tile_y;
         if(map_representation[s_x, s_y] > tile_threshold_to_move)
         {
             print("placed start tile on a tile deemed unmovable");
-            return;
+            return path;
         }
 
         int e_x = e_i / num_squares_per_tile_x, e_y = e_j - 4 / num_squares_per_tile_y;
         if (map_representation[e_x, e_y] > tile_threshold_to_move)
         {
             print("placed end tile on a tile deemed unmovable");
-            return;
+            return path;
         }
 
         // now actually do a*
         print("I think I can I think I can!");
+
+        List<Point> open_points = new List<Point>();
+        open_points.Add(new Point(s_x, s_y));
+
+        List<Point> close_points = new List<Point>();
+        int count = 0;
+
+        while(open_points.Count > 0 && count < 100)
+        {
+            count++; // there for debugging so we don't hit infinite loop
+
+            // consider the best node in the open list
+            Point best_point = FindSmallestInList(open_points);
+
+            // if this node is the goal
+            if(best_point.x == e_x && best_point.y == e_y)
+            {
+                break;
+            }
+
+            // else move the current node to the closest list and conisder all of its neighbors
+            open_points.Remove(best_point);
+
+            float cur_g = 0.0f; // do this!!!!
+
+            List<Point> neighbors = GetAdjacentOpenTiles(best_point.x, best_point.y);
+            foreach(Point p in neighbors)
+            {
+                Point successor = p;
+                successor.g = best_point.g + 1;
+                successor.h = HeuristicEuclidianDist(successor.x, successor.y, e_x, e_y);
+                successor.f = successor.g + successor.h;
+
+                //MIT psuedocode
+
+                // I think this should find a point that has same x,y as sucessor?? - it does not
+                // code modified from https://msdn.microsoft.com/en-us/library/5kthb929(v=vs.110).aspx
+                Point? same = open_points.FindLast(
+                    delegate (Point pt)
+                    {
+                        return p.x == successor.x && p.y == successor.y;
+                    });
+
+                if(same != null)
+                {
+                    //if a node with the same position as successor is in the OPEN list \
+                    //which has a lower f than successor, skip this successor
+                    if(same.Value.f < successor.f)
+                    {
+                        continue;
+                    }
+                    
+                }
+
+                //if a node with the same position as successor is in the CLOSED list \ 
+                //    which has a lower f than successor, skip this successor
+                same = close_points.FindLast(
+                    delegate (Point pt)
+                    {
+                        return p.x == successor.x && p.y == successor.y;
+                    });
+                if(same != null && same.Value.f < successor.f)
+                {
+                    continue;
+                }
+
+                //otherwise, add the node to the open list
+                open_points.Add(successor);
+
+
+                //PROF SLIDE PSUEDOCODE:
+                //if this neighbor is in the closest list and our current g value is lower
+                // update the neighbor with the new, lower g value
+                // change the neighbor's parent to our current node
+
+
+                // else if this neighbor is in the open list and our current g value is lower
+                // update the neighbor iwth the new, lower g value
+                // change the neighbor's parent to our current node
+
+                // else the neight is not iin either the open or closes list
+                // add the neighbor to the open list and ste its g value
+            }
+
+            close_points.Add(best_point);
+
+
+        }
+
+        print("found a path! count = " + count);
+        return path;
+    }
+
+    private Point FindSmallestInList(List<Point> l)
+    {
+        // never will have an empty list
+        Point smallest_point = new Point(0,0);
+        float smallest_cost = float.MaxValue;
+        foreach(Point p in l)
+        {
+            if(p.f < smallest_cost)
+            {
+                smallest_point = p;
+                smallest_cost = p.f;
+            }
+        }
+        return smallest_point;
+    }
+
+    private List<Point> GetAdjacentOpenTiles(int x, int y)
+    {
+        List<Point> adjacent = new List<Point>();
+
+        // left
+        if(x > 0 && map_representation[x-1, y] < tile_threshold_to_move)
+        {
+            adjacent.Add(new Point(x-1,y));
+        }
+
+        // right
+        if (x < width - 1 && map_representation[x + 1, y] < tile_threshold_to_move)
+        {
+            adjacent.Add(new Point(x + 1, y));
+        }
+
+        // up
+        if (y > 0 && map_representation[x, y-1] < tile_threshold_to_move)
+        {
+            adjacent.Add(new Point(x, y - 1));
+        }
+
+        // down
+        if (y < height-1 && map_representation[x, y + 1] < tile_threshold_to_move)
+        {
+            adjacent.Add(new Point(x, y + 1));
+        }
+        return adjacent;
     }
 
     private float HeuristicEuclidianDist(int x, int y, int endX, int endY)
