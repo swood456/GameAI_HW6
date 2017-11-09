f0bb77b3e19570aee7f905f8f3df6473fbcebdf0
swood456 <crazedsportsfan@gmail.com>
2017-11-08 22:28:05 -0500

    added a button that doesn't work to swap level

---
+++ Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour {
+    public string next_level_name;
+	
+	public void GoToNextLevel()
+    {
+        SceneManager.LoadScene(next_level_name);
+    }
+}
